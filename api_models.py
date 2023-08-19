e88c011fae328e33a576496b7a4197f0657c08a7
splanky0314 <splanky0314@gmail.com>
2023-08-19 18:02:50 +0900

    splanky0314: models 충돌 이슈 해결 & check chellenge 완성

--- api_models.py
+++ api_models.py
@@ -4,21 +4,18 @@ from sqlmodel import Field, SQLModel
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field(index=True)
-    file: str = Field(index=True, default=None)
 
 class Photo(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    file_url: str
-
-class UserPhotoLink(SQLModel, table=True):
     user_id: Optional[int] = Field(
-        default=None, foreign_key="user.id", primary_key=True
+        default=None, foreign_key="user.id"
     )
-    photo_id: Optional[int] = Field(
-        default=None, foreign_key="photo.id", primary_key=True
+    challenge_id: Optional[int] = Field(
+        default=None, foreign_key="challenge.id"
     )
+    photo_url: str
 
-class Challenge(SQLModel):
+class Challenge(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     challenge_name: str = Field(index=True)
-    answer: set
+    answer: str
--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from typing import Optional, Annotated
-from fastapi import FastAPI, File, UploadFile, Body
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from fastapi import FastAPI, UploadFile, Body
+from sqlmodel import Session, SQLModel, create_engine, select
 from pathlib import Path
 import datetime
 import os, sys
-
+from api_models import User, Photo, Challenge
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from yolov5 import detect
 
-from models import User, Photo, UserPhotoLink, Challenge
-
 PROJECT_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = Path(__file__).resolve().parent
-STATIC_DIR = os.path.join(BASE_DIR,'static/')
-IMG_DIR = os.path.join(STATIC_DIR,'images/')
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+IMG_DIR = os.path.join(STATIC_DIR,'images')
 SERVER_IMG_DIR = os.path.join('http://localhost:8000/','static/','images/')
 
 sqlite_file_name = "database.db"
@@ -25,6 +23,28 @@ engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
+create_db_and_tables()
+
+app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+"""
+Test DB
+"""
+# with Session(engine) as session:
+#     challenge = Challenge(challenge_name="recycle", answer="metal, cardboard")
+#     session.add(challenge)
+#     session.commit()
+#     session.refresh(challenge)
+
+"""
+Function Defination
+"""
+
 def create_folder(root) -> None:
     try:
         if not os.path.exists(root):
@@ -32,10 +52,37 @@ def create_folder(root) -> None:
     except OSError:
         print("ERROR: Create Folder Failed :"+root)
 
-def challenge_check(photo_location: str, challenge_name:str) -> dict:
+def _get_or_create_user_by_username(username:str) -> User:
+    with Session(engine) as session:
+        statement = select(User).where(User.username == username)
+        user = session.exec(statement).first()
+
+        if user == None:
+            user = User(username=username)
+            session.add(user)
+            session.commit()
+            session.refresh(user)
+    return user
+
+def _create_photo(user:User, challenge:Challenge, photo_url:str):
+    with Session(engine) as session:
+        photo = Photo(user_id=user.id, challenge_id=challenge.id, photo_url=photo_url)
+        session.add(photo)
+        session.commit()
+        session.refresh(photo)
+    return photo
+
+def _get_challenge_by_challenge_name(challenge_name:str) -> Challenge:
+    with Session(engine) as session:
+        statement = select(Challenge).where(Challenge.challenge_name == challenge_name)
+        challenge = session.exec(statement).first()
+    return challenge
+
+def check_challenge(photo_location: str, challenge_name:str) -> dict:
+    photo_location = Path(photo_location)
     objects:set = detect.run(
         weights=['../yolov5/runs/train/result_trash/weights/best.pt'], 
-        source='../'+photo_location, 
+        source=photo_location, 
         data=os.path.join(PROJECT_DIR, 'yolov5/data/coco128.yaml'), 
         imgsz=[640, 640], conf_thres=0.25, iou_thres=0.45, max_det=1000, device='', 
         view_img=False, save_txt=False, save_conf=False, save_crop=False, 
@@ -44,10 +91,12 @@ def challenge_check(photo_location: str, challenge_name:str) -> dict:
         name='exp', exist_ok=False, line_thickness=3, hide_labels=False, hide_conf=False, half=False, dnn=False, vid_stride=1
     )
 
-    answer:set = ...
+    challenge = _get_challenge_by_challenge_name(challenge_name)
+    answer = challenge.answer.split(", ")
+    answer = set(answer)
     founded = answer.intersection(objects)
-    missing = answer.subtraction(objects)
-    
+    missing = answer.difference(objects)
+    print(objects)
     if(missing):
         result = {
             "success":False,
@@ -61,36 +110,47 @@ def challenge_check(photo_location: str, challenge_name:str) -> dict:
 
     return result
 
-app = FastAPI()
-
-@app.post("/challenge/")
-def challenge(user:User, file: UploadFile):
-    date = datetime.datetime.now().strftime("%Y-%m-%d")
+# for Test
+@app.get("/data")
+async def data():
+    with Session(engine) as session:
+        users = session.exec(select(User)).all()
+        challenges = session.exec(select(Challenge)).all()
+        photos = session.exec(select(Photo)).all()
+        result = {
+            "users": users,
+            "challenges": challenges,
+            "photos": photos
+        }
+        return result
+    
 
 @app.post('/upload-images')
-async def upload_board(username:Annotated[str, Body(embed=True, alias="username")], challenge:Annotated[str, Body(embed=True, alias="challenge")], file:UploadFile):
-    root = os.path.join(IMG_DIR, username, challenge)
+async def upload_images(username:str, challenge_name:str, file:UploadFile):
+    root = os.path.join(IMG_DIR, username, challenge_name)
+    print(root)
     create_folder(root)
     
     """ 
     Save Image 
     """
     saved_file_name = datetime.datetime.now().strftime("%Y%m%d%H%M%S")+".png"
-    file_location = os.path.join(root, saved_file_name)
+    photo_url = os.path.join(root, saved_file_name)
 
-    with open(file_location, "wb+") as file_object:
+    with open(photo_url, "wb+") as file_object:
         file_object.write(file.file.read())
 
     """
-    Challenge Chack
+    Create Photo
+    """
+    user = _get_or_create_user_by_username(username)
+    challenge = _get_challenge_by_challenge_name(challenge_name)
+    _create_photo(user, challenge, photo_url)
+
+    """
+    Check Challenge
     """
 
-    result={
-        "success": True,
-        "message": {
-            
-        }
-    }
+    result=check_challenge(photo_url, challenge_name)
     return result
-
 
